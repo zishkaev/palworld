ccd14711759d7eaa2961305ee7a73b9c8488219c
Orl-a-n-Do <luckypirat88@mail.ru>
2024-02-29 19:18:47 +0300

    Revert "Add Fading of The Screen"
    This reverts commit fd1723de801529451c3bb6ed7057026a7cd2547e.

--- Assets/#Project/Scripts/Loader.cs
+++ Assets/#Project/Scripts/Loader.cs
@@ -1,31 +1,9 @@
-using System.Collections;
 using UnityEngine;
 
 public class Loader : MonoBehaviour {
 	public SceneType scene;
-    [SerializeField] LevelFadingScreen levelFadingScreen;
-
-
-    void Awake()
-    {
-
-        //levelFadingScreen = new LevelFadingScreen();
-
-
-    }
-
 
 	public void Load() {
-        
-        levelFadingScreen.FadeToLevel();
-        // SceneController.instance.LoadScene(scene);
-        Invoke(nameof(LoadScene), 3);
-    }
-
-    
-
-    public void LoadScene() 
-    {
-        SceneController.instance.LoadScene(scene);
-    }
+		SceneController.instance.LoadScene(scene);
+	}
 }
