e4fb1e0da84c18cde0cc6592c64cdde2692e1756
Orl-a-n-Do <luckypirat88@mail.ru>
2024-02-29 19:16:54 +0300

    Revert "Addd new House"
    This reverts commit 0fdae220454c3dcbb3f8f43ef0f63435f0695be3.

---
+++ Assets/Menu Prallac/AnimationsScripts/ImageMovement.cs
@@ -0,0 +1,26 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+
+
+public class ImageMovement : MonoBehaviour
+{
+    public Image image;
+    public float moveDistance = 200f;
+    public float moveDuration = 2f;
+
+    private Vector3 originalPos;
+
+    private void Start()
+    {
+        // Сохраняем исходную позицию картинки
+        originalPos = image.rectTransform.position;
+
+        // Запускаем плавное перемещение картинки вправо
+        image.rectTransform.DOMoveX(originalPos.x + moveDistance, moveDuration)
+            .SetEase(Ease.InOutQuad)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+}
